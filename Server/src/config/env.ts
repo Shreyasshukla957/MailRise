e60aa12f626e11fb78e3681ce13b6aa42e345d26
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-07-14 00:00:45 +0530

    feat: add env configuration

---
+++ Server/src/config/env.ts
@@ -0,0 +1,22 @@
+
+import dotenv from "dotenv"
+dotenv.config();
+
+// Centralized Enviornment Variable
+
+const env = ["PORT", "MONGODB_URI", "JWT_SECRET", "Client_Id", "Client_Secret", "Redirect_Uris"];
+
+export const requiredEnv = {
+    "PORT": process.env.PORT || "3000",
+    "MONGODB_URI": process.env.MONGODB_URI as string,
+    "JWT_SECRET": process.env.JWT_SECRET as string,
+    "client_id": process.env.Client_Id as string,
+    "client_secret": process.env.Client_Secret as string,
+    "redirect_uris": process.env.Redirect_Uris as string,
+}
+
+env.forEach((env) => {
+    if (!process.env[env]) {
+        throw new Error(`Variable ${env} is missing`);
+    }
+})
