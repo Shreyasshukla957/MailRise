2412571a57bb42190dc9c65e28f8253b388d28c6
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-08-16 23:49:46 +0530

    feat: add email send route with auth guard

--- Server/src/routes/email.routes.ts
+++ Server/src/routes/email.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/authmiddleware.js";
 import { generateEmail } from "../controllers/email.controller.js";
+import { sendEmail } from "../controllers/email.controller.js";
 
 
 const emailRouter: Router = Router();
 
-emailRouter.post("/draft", generateEmail);
+emailRouter.post("/draft",authMiddleware, generateEmail);
+emailRouter.post("/send",authMiddleware,sendEmail);
 
 export default emailRouter;
