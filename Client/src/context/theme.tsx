72b88becf6c89d4f058d1ad356823eb720bcee7a
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-08-19 22:59:37 +0530

    feat: add theme toggle

---
+++ Client/src/context/theme.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { Sun, Moon } from "lucide-react";
+
+export const Theme = ({
+  className,
+}: {
+  className?: string;
+}): React.JSX.Element => {
+  const [dark, setdark] = useState<boolean>(
+    localStorage.getItem("mailrise-theme") !== "light"
+  );
+
+  const toggleTheme = () => {
+    const theme = !dark;
+    setdark(theme);
+
+    document.documentElement.classList.toggle("dark", theme);
+    localStorage.setItem("mailrise-theme", theme ? "dark" : "light");
+  };
+
+  return (
+    <div className="">
+      <button
+        onClick={toggleTheme}
+        className="border-default bg-card/60 group hover:border-themeborder relative flex cursor-pointer items-center justify-center rounded-md border p-4 transition-all duration-200 outline-none"
+      >
+        <Sun
+          size={35}
+          className={`-[transform,opacity] absolute p-2 duration-100 ${
+            dark
+              ? "text-theme scale-100 rotate-0 opacity-100"
+              : "scale-0 rotate-45 opacity-0"
+          }`}
+        />
+        <Moon
+          size={35}
+          className={`absolute p-2 transition-[transform,opacity] duration-100 ${
+            dark
+              ? "scale-0 rotate-45 opacity-0"
+              : "text-theme group-hover:text-themeborder scale-100 rotate-0 opacity-100"
+          }`}
+        />
+      </button>
+    </div>
+  );
+};
