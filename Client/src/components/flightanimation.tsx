0b84474247307acd59ca0a7112d670208ef049f3
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-08-20 18:30:04 +0530

    feat: add flight animation component

---
+++ Client/src/components/flightanimation.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { cn } from "../lib/utils";
+import { motion } from "motion/react";
+
+export const Flightanimation = ({
+  className,
+}: {
+  className?: string;
+}): React.JSX.Element => {
+  const flightpath =
+    "M471.191 614.718C463.691 622.718 483.191 507.041 456.691 467.118C430.191 427.195 56.6908 518.618 25.1908 450.218C-6.30919 381.818 -7.80929 187.218 25.1908 144.609C58.1909 102.001 460.691 180.888 481.691 137.218C502.691 93.5486 481.691 0.218262 481.691 0.218262";
+  return (
+    <div className={cn("", className)}>
+      <svg
+        width="493"
+        height="617"
+        viewBox="-50 -50 600 720"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className=""
+      >
+        <path
+          d={flightpath}
+          stroke="currentColor"
+          strokeWidth={2}
+          fill="none"
+          strokeDasharray="8 8"
+          id="flightroute"
+          className="text-subtle"
+        />
+        <motion.svg
+          style={{
+            offsetPath: `path("${flightpath}")`,
+            offsetRotate: "auto 90deg",
+          }}
+          animate={{ offsetDistance: "100%",scale:0.8 }}
+          initial={{ offsetDistance: "0%" , scale:0.8 }}
+          transition={{
+            duration: 6,
+            repeat: Infinity,
+            repeatDelay: 4,
+            ease: "easeInOut",
+          }}
+          className="drop-shadow-[0_0_0px_var(--color-glow)]"
+        >
+          <path d="M 0 -35 L -26 25 L 0 12 Z" fill="var(--color-bright)" />
+
+          {/* 🌑 2. Right Wing (Shadow Side) */}
+          <path d="M 0 -35 L 0 12 L 26 25 Z" fill="var(--color-focus)" />
+
+          {/* ⚡ 3. Center Fold Spine (Crisp White Crease!) */}
+          <line
+            x1="0"
+            y1="-35"
+            x2="0"
+            y2="12"
+            stroke="white"
+            strokeWidth="1.5"
+            strokeOpacity="0.9"
+          />
+        </motion.svg>
+      </svg>
+    </div>
+  );
+};
