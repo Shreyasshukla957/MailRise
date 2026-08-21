3663ed812dacd0d8798e2125c5e5204612fc6694
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-08-21 21:10:12 +0530

    fix: adjust avatar position and sender labels

--- Client/src/components/hero.tsx
+++ Client/src/components/hero.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { cn } from "../lib/utils";
-import { motion } from "motion/react";
+// import { motion } from "motion/react";
 import { Flightanimation } from "./flightanimation";
 import { Header } from "./header";
 import { Dashboard } from "./dashboard";
@@ -19,7 +19,7 @@ export const Hero = ({
         <Flightanimation className="pointer-events-none absolute inset-0 z-0 -translate-x-3/5 translate-y-1/8 rotate-180" />
         <img
           src="./img-avatar.jpg"
-          className="absolute top-1 -left-24 size-15 rounded-full"
+          className="absolute top-17 -left-24 size-15 rounded-full"
         ></img>
         <img
           src="./img-avatar2.jpg"
@@ -33,7 +33,7 @@ export const Hero = ({
           src="./img-avatar4.jpg"
           className="absolute -right-24 -bottom-32 size-15 rounded-full"
         ></img>
-        <span className="font-caveat text-subtle hover:text-focus absolute -top-8 -left-22 text-2xl">
+        <span className="font-caveat text-subtle hover:text-focus absolute top-5 -left-22 text-2xl">
           Sender
         </span>
         <span className="font-caveat text-subtle hover:text-focus absolute -right-25 -bottom-40 text-2xl">
