d2b6969edc37bdc5ebeaad6e6c83f76a18aedf0d
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-08-23 23:31:32 +0530

    style: new path for animation

--- Client/src/components/hero.tsx
+++ Client/src/components/hero.tsx
@@ -10,20 +10,23 @@ export const Hero = ({
 }: {
   className?: string;
 }): React.JSX.Element => {
+
+  const path = "M183.017 804.799C224.808 765.27 234.523 727.142 238.393 669.749C243.679 591.337 200.327 428.357 150.182 488.87C120.54 524.642 97.1829 575.889 111.406 620.116C144.716 723.693 366.463 603.27 378.377 495.123C387.622 411.21 228.651 292.137 228.651 292.137C228.651 292.137 150.589 195.746 108.567 241.344C66.5458 286.942 12.0099 318.42 2.69744 359.533C-32.1984 513.592 360.054 0.298571 360.054 0.298571"
+  const path1 = "M471.191 614.718C463.691 622.718 483.191 507.041 456.691 467.118C430.191 427.195 56.6908 518.618 25.1908 450.218C-6.30919 381.818 -7.80929 187.218 25.1908 144.609C58.1909 102.001 460.691 180.888 481.691 137.218C502.691 93.5486 481.691 0.218262 481.691 0.218262"
   return (
     <div className={cn("flex flex-col items-center justify-center", className)}>
       <Header className="font-rubik max-w-6xl"></Header>
       <DashboardBorder className="relative">
         <Dashboard className="font-rubik border-primary/10 border-px relative z-10 w-full max-w-4xl border"></Dashboard>
-        <Flightanimation className="pointer-events-none absolute inset-0 z-0 translate-x-3/5 translate-y-1/8" />
-        <Flightanimation className="pointer-events-none absolute inset-0 z-0 -translate-x-3/5 translate-y-1/8 rotate-180" />
+        <Flightanimation className="pointer-events-none absolute inset-0 z-0 translate-x-10/13 translate-y-2/8 " props={path}/>
+        <Flightanimation className="pointer-events-none absolute inset-0 z-0 -translate-x-3/5 translate-y-1/8 rotate-180" props={path1}/>
         <img
           src="./img-avatar.jpg"
           className="absolute top-17 -left-24 size-15 rounded-full"
         ></img>
         <img
           src="./img-avatar2.jpg"
-          className="absolute top-10 -right-24 size-15 rounded-full"
+          className="absolute top-30 -right-43 size-15 rounded-full"
         ></img>
         <img
           src="./img-avatar3.jpg"
@@ -31,15 +34,15 @@ export const Hero = ({
         ></img>
         <img
           src="./img-avatar4.jpg"
-          className="absolute -right-24 -bottom-32 size-15 rounded-full"
+          className="absolute -right-18 -bottom-52 size-15 rounded-full"
         ></img>
         <span className="font-caveat text-subtle hover:text-focus absolute top-5 -left-22 text-2xl">
           Sender
         </span>
-        <span className="font-caveat text-subtle hover:text-focus absolute -right-25 -bottom-40 text-2xl">
+        <span className="font-caveat text-subtle hover:text-focus absolute -right-19 -bottom-62 text-2xl">
           Sender
         </span>
-        <span className="font-caveat text-subtle absolute top-0 -right-25 text-2xl">
+        <span className="font-caveat text-subtle absolute top-18 -right-45 text-2xl">
           Recipient
         </span>
         <span className="font-caveat text-subtle absolute -bottom-35 -left-26 text-2xl">
