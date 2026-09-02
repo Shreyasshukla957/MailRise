ceb8e71f03cd3017535502b93094a2f83bb9cff9
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-09-02 22:08:06 +0530

    feat:created login component

---
+++ Client/src/components/Login.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { cn } from "../lib/utils";
+import { FcGoogle } from "react-icons/fc";
+import { CiLock } from "react-icons/ci";
+
+export const Login = ({ className }: { className?: String }) => {
+  return (
+    <div
+      className={cn(
+        "bg-hero flex h-full min-h-screen w-full items-center justify-center overflow-hidden from-90% mask-x-from-80% bg-cover",
+        className
+      )}
+    >
+     
+    </div>
+  );
+};
+
+
