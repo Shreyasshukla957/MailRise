3ad91d86ca74d39abc398cad5ea5bd8afb7cd1ee
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-09-09 23:48:50 +0530

    feat:updated with react router

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,28 +1,29 @@
 import { Navbar } from "./components/navbar";
-import { Container } from "./components/container";
-import { Hero } from "./components/hero";
 import "./App.css";
-import { cn } from "./lib/utils";
-import { Svgworks } from "./components/svgworks";
-import { Works } from "./components/Works";
-import { UNDERSTAND } from "./components/svgworks";
-import { Feature } from "./components/feature";
 import { Routes, Route } from "react-router";
 import { Login } from "./components/Login";
 import { Landing } from "./components/Landing";
+import { Sidebar } from "./pages/Sidebar";
+import { Workspace } from "./pages/Workspace";
+import { Dashboard } from "./pages/Dashboard";
+import { Inbox } from "./pages/Inbox";
 
 function App() {
   return (
-    <div className="bg-screen flex min-h-screen w-full min-w-screen justify-center">
+    <div className="bg-screen flex min-h-screen w-full justify-center">
       <Routes>
-        <Route path="/" element={<Landing />}>
-        </Route>
+        <Route path="/" element={<Landing />} />
+
         <Route path="/login" element={<Login />} />
+
+        <Route element={<Sidebar />}>
+          <Route path="/workspace" element={<Workspace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/inbox" element={<Inbox />} />
+        </Route>
       </Routes>
     </div>
   );
 }
 
 export default App;
-
-
